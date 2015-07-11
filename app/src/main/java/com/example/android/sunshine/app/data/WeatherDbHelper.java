d7d9f7b9ec35a7baadf7741ccf28bea322805577
Benjamin <bysy@users.noreply.github.com>
2015-07-10 22:43:27 -0500

    fix: Use default conflict resolution

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
@@ -41,12 +41,10 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
                 LocationEntry._ID + " " + "INTEGER PRIMARY KEY," +
 
-                LocationEntry.COLUMN_LOCATION_SETTING+" "+"TEXT NOT NULL,"+" "+
+                LocationEntry.COLUMN_LOCATION_SETTING+" "+"TEXT UNIQUE NOT NULL,"+" "+
                 LocationEntry.COLUMN_CITY_NAME +" "+"TEXT NOT NULL,"+" "+
                 LocationEntry.COLUMN_COORD_LAT +" "+"TEXT NOT NULL,"+" "+
-                LocationEntry.COLUMN_COORD_LONG+" "+"TEXT NOT NULL,"+" "+
-
-                "UNIQUE ("+LocationEntry.COLUMN_LOCATION_SETTING+") ON CONFLICT REPLACE);";
+                LocationEntry.COLUMN_COORD_LONG+" "+"TEXT NOT NULL"+");";
 
         sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
 
