b161364582efd19daf979efcdb4b1dab72cc812d
Benjamin <bysy@users.noreply.github.com>
2015-07-24 18:11:09 -0500

    feat: Bind data to new item view

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -2,10 +2,12 @@ package com.example.android.sunshine.app;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.media.Image;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.sunshine.app.data.WeatherContract;
@@ -35,11 +37,32 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
+        final long dateInMillis = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        final float maxC = cursor.getFloat(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        final float minC = cursor.getFloat(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        final int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_CONDITION_ID);
+        final String desc = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
 
-        TextView tv = (TextView) view.findViewById(R.id.list_item_forecast_textview);
-        if (tv==null) { return; }
-        tv.setText(ForecastFragment.convertCursorRowToUXFormat(cursor, Utility.isMetric(mContext)));
+        final boolean isMetric = Utility.isMetric(mContext);
+
+        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        if (dateView!=null) {
+            dateView.setText(Utility.getFriendlyDayString(mContext, dateInMillis));
+        }
+        TextView maxTemperatureView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        if (maxTemperatureView!=null) {
+            maxTemperatureView.setText(Utility.formatTemperature(maxC, isMetric));
+        }
+        TextView minTemperatureView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        if (minTemperatureView!=null) {
+            minTemperatureView.setText(Utility.formatTemperature(minC, isMetric));
+        }
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        if (iconView!=null) {
+            // TODO: Load actual image for weatherId
+            iconView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_launcher));
+        }
+        TextView descView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        if (descView!=null) { descView.setText(desc); }
     }
 }
