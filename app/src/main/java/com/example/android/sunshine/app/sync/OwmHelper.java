bysy/Sunshine

commit 45dae2bd2cbdec969d3817011394687ba8aadfb7

feat: Communicate common error cases

app/src/main/java/com/example/android/sunshine/app/sync/OwmHelper.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.text.format.Time;
 import android.util.Log;
 
+import com.example.android.sunshine.app.Utility;
 import com.example.android.sunshine.app.data.WeatherContract;
 
 import org.json.JSONArray;
@@ -33,12 +34,20 @@ public class OwmHelper {
 
     /** Update context's weather content provider with fresh data from OWM. */
     static public void update(Context context, String locationQuery) {
+        Utility.setLocationStatus(context, SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
         String forecastJsonStr = OwmHelper.retrieveForecastJsonString(locationQuery);
-        if (forecastJsonStr==null) return;
+        if (forecastJsonStr==null) {
+            Utility.setLocationStatus(context, SunshineSyncAdapter.LOCATION_STATUS_SERVER_DOWN);
+            Log.d(LOG_TAG, "Sunshine weather update failed: couldn't get a response from server");
+            return;
+        }
 
         List<ContentValues> forecast = OwmHelper.parseJsonForecast(context, forecastJsonStr, locationQuery);
         // add to database
-        if (!forecast.isEmpty()) {
+        if (forecast.isEmpty()) {
+            Utility.setLocationStatus(context, SunshineSyncAdapter.LOCATION_STATUS_SERVER_INVALID);
+            Log.d(LOG_TAG, "Sunshine weather update failed: invalid server response");
+        } else {
             context.getContentResolver().bulkInsert(
                     WeatherContract.WeatherEntry.CONTENT_URI,
                     forecast.toArray(new ContentValues[forecast.size()]));
@@ -49,10 +58,9 @@ public class OwmHelper {
                     WeatherContract.WeatherEntry.CONTENT_URI,
                     WeatherContract.WeatherEntry.COLUMN_DATE + " <= ?",
                     new String[]{Long.toString(dayTime.setJulianDay(julianStartDay - 1))});
+            Utility.setLocationStatus(context, SunshineSyncAdapter.LOCATION_STATUS_OK);
             Log.d(LOG_TAG, "Sunshine weather update complete: " + forecast.size() + " inserted.\n" +
                     "\tDeleted " + numDeleted + " record(s).");
-        } else {
-            Log.d(LOG_TAG, "Sunshine weather update failed");
         }
     }
 
@@ -112,7 +120,8 @@ public class OwmHelper {
             // Possible parameters are avaiable at OWM's forecast API page, at
             // http://openweathermap.org/API#forecast
             final String FORECAST_BASE_URL =
-                    "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                    "http://google.com/ping?";
+                    //"http://api.openweathermap.org/data/2.5/forecast/daily?";
             final String QUERY_PARAM = "q";
             final String FORMAT_PARAM = "mode";
             final String UNITS_PARAM = "units";

app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.sunshine.app;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -29,7 +30,10 @@ import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class ForecastFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<Cursor>,
+        SharedPreferences.OnSharedPreferenceChangeListener
+{
     private static final String TAG = ForecastFragment.class.getSimpleName();
     private static final String BUNDLE_POSITION_KEY = "BUNDLE_POSITION_KEY";
     private static final int FORECAST_LOADER_ID = 0;
@@ -74,6 +78,14 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         if (mForecastAdapter!=null) {
             mForecastAdapter.setUseTodayLayout(useTodayLayout);
         }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (!key.equals(getString(R.string.location_status_key))) {
+            return;
+        }
+        updateEmptyView();
     }
 
     /**
@@ -189,10 +201,24 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateEmptyView() {
-        if (mForecastAdapter.isEmpty()) {
-            if (!Utility.isNetworkAvailable(getActivity())) {
-                mEmptyTextView.setText(getString(R.string.empty_forecast_text) +
-                        "\n" + getString(R.string.no_connectivity_text));
+        if (!mForecastAdapter.isEmpty()) {
+            return;
+        }
+        final String base = getString(R.string.empty_forecast_text);
+
+        if (!Utility.isNetworkAvailable(getActivity())) {
+            mEmptyTextView.setText(base + "\n" + getString(R.string.no_connectivity_text));
+        } else {
+            switch (Utility.getLocationStatus(getActivity())) {
+                case SunshineSyncAdapter.LOCATION_STATUS_SERVER_DOWN:
+                    mEmptyTextView.setText(base + "\n" + getString(R.string.server_down_text));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_SERVER_INVALID:
+                    mEmptyTextView.setText(base + "\n" + getString(R.string.server_response_invalid_text));
+                    break;
+                case SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                default:
+                    mEmptyTextView.setText(base + "\n" + getString(R.string.server_unknown_error_text));
             }
         }
     }

app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -17,11 +17,12 @@ package com.example.android.sunshine.app;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.text.format.Time;
+
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -37,6 +38,19 @@ public class Utility {
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         final NetworkInfo activeNetworkInfo = cm.getActiveNetworkInfo();
         return activeNetworkInfo!=null && activeNetworkInfo.isConnected();
+    }
+
+    public static void setLocationStatus(Context context, @SunshineSyncAdapter.LocationStatus int status) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final String key = context.getString(R.string.location_status_key);
+        prefs.edit().putInt(key, status).commit();
+    }
+
+    public static @SunshineSyncAdapter.LocationStatus int getLocationStatus(Context context) {
+        final String key = context.getString(R.string.location_status_key);
+        //noinspection ResourceType  // always location status due to setter and default
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getInt(key, SunshineSyncAdapter.LOCATION_STATUS_UNKNOWN);
     }
 
     /**

app/src/main/java/com/example/android/sunshine/app/sync/SunshineSyncAdapter.java
@@ -17,9 +17,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.IntDef;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.text.format.Time;
 import android.util.Log;
 
 import com.example.android.sunshine.app.MainActivity;
@@ -27,9 +27,22 @@ import com.example.android.sunshine.app.R;
 import com.example.android.sunshine.app.Utility;
 import com.example.android.sunshine.app.data.WeatherContract;
 
-import java.util.Calendar;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({LOCATION_STATUS_OK,
+             LOCATION_STATUS_SERVER_DOWN,
+             LOCATION_STATUS_SERVER_INVALID,
+             LOCATION_STATUS_UNKNOWN})
+    public @interface LocationStatus {}
+
+    public static final int LOCATION_STATUS_OK = 0;
+    public static final int LOCATION_STATUS_SERVER_DOWN = 1;
+    public static final int LOCATION_STATUS_SERVER_INVALID = 2;
+    public static final int LOCATION_STATUS_UNKNOWN = 3;
+
     private static final int HOUR_IN_SEC = 60 * 60;
     private static final int SYNC_INTERVAL = 3 * HOUR_IN_SEC;
     private static final int SYNC_FLEXTIME = 2 * HOUR_IN_SEC;

app/src/main/res/values/strings.xml
@@ -65,6 +65,7 @@
     <string name="pref_units_key">units</string>
     <string name="pref_units_metric">metric</string>
     <string name="pref_units_imperial">imperial</string>
+    <string name="location_status_key">location_status_key</string>
     <!-- Strings related to Notification preference -->
     <string name="pref_last_notification">last_notification</string>
     <string name="pref_notifications_enabled">notificationsEnabled</string>
@@ -72,4 +73,7 @@
     <!-- SyncAdapter related -->
     <string name="sync_account_type">sunshine.example.com</string>
     <string name="content_authority">com.example.android.sunshine.app</string>
+    <string name="server_down_text">Weather server is down</string>
+    <string name="server_response_invalid_text">Couldn\'t understand server response</string>
+    <string name="server_unknown_error_text">An unexpected error occurred while syncing</string>
 </resources>

commit 4ec439aa6eb61c170d3a86dc69b79253d503305b

fix: Correct URL
Fix wrong URL introduced in 45dae2bd2cbdec969d3817011394687ba8aadfb7

app/src/main/java/com/example/android/sunshine/app/sync/OwmHelper.java
@@ -120,8 +120,7 @@ public class OwmHelper {
             // Possible parameters are avaiable at OWM's forecast API page, at
             // http://openweathermap.org/API#forecast
             final String FORECAST_BASE_URL =
-                    "http://google.com/ping?";
-                    //"http://api.openweathermap.org/data/2.5/forecast/daily?";
+                    "http://api.openweathermap.org/data/2.5/forecast/daily?";
             final String QUERY_PARAM = "q";
             final String FORMAT_PARAM = "mode";
             final String UNITS_PARAM = "units";